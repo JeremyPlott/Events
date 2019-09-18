d7df11390b60b5c98d5ce4c97845c67aaa27e4a9
Greg Doud <gpdoud@gmail.com>
2019-09-18 16:15:10 -0400

    Add project files.

---
+++ EventsConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using EventsLibrary;
+using System;
+
+namespace EventsConsole {
+    class Program {
+
+        void NbrMod5(object sender, EventArgs e) {
+            Console.WriteLine("The nbr is evently divisible by 5.");
+        }
+        void Run() {
+            var ue = new UserEvent();
+            ue.RaiseEvent += NbrMod5;
+            ue.Add1();
+            ue.Add1();
+            ue.Add1();
+            ue.Add1();
+            ue.Add1();
+        }
+        static void Main(string[] args) {
+            (new Program()).Run();
+        }
+    }
+}
---
+++ EventsLibrary/UserEvent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventsLibrary {
+
+    public class UserEvent {
+
+        private int nbr = 0;
+
+        public delegate void EventHandler(object sender, EventArgs e);
+
+        public event EventHandler RaiseEvent;
+
+        public void Add1() {
+            nbr++;
+            if(nbr % 5 == 0) {
+                FireEvent();
+            }
+        }
+
+        private void FireEvent() {
+            var e = new EventArgs();
+            OnFireEvent(e);
+        }
+
+        public virtual void OnFireEvent(EventArgs e) {
+            if(RaiseEvent != null)
+                RaiseEvent(this, e);
+        }
+    }
+}
